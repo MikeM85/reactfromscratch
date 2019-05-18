0a94502e3daa8b7c205389968f573522cbec514d
Mike <mikeinhistory@gmail.com>
2019-05-18 11:32:59 -0500

    controllers files updated, proxy still not working

--- loginplus/server.js
+++ loginplus/server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/coolhands", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/coolhandapp", { useNewUrlParser: true });
 
 //Passport Middleware
 app.use(passport.initialize());
